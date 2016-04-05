b35ca7a86c378bae3f8faff38679d18b5f754711
Alexandre Oliveira <rockytvbr@gmail.com>
2016-04-04 22:15:04 -0300

    Add license notice to source file

--- src/dicepyware.py
+++ src/dicepyware.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2016 Alexandre Oliveira
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU General Public License along 
+# with this program. If not, see http://www.gnu.org/licenses/.
+
 __version__ = '0.1.0'
 
 class InvalidPassphraseLength(Exception):
